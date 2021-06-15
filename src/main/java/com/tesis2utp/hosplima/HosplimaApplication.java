f9e7128146a43cdf1fa892b3bd77bada82191eb9
carldevoos <carl.devoos@gmail.com>
2021-06-14 22:16:07 -0500

    Añade proyecto base

---
+++ src/main/java/com/tesis2utp/hosplima/HosplimaApplication.java
@@ -0,0 +1,13 @@
+package com.tesis2utp.hosplima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosplimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosplimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesis2utp/hosplima/HosplimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis2utp.hosplima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosplimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
