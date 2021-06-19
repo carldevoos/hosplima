9f9f34bbef45d362ac5eaf76766522ba5bdb139f
carldevoos <carl.devoos@gmail.com>
2021-06-19 00:31:40 -0500

    Añade CrossOrigin

--- src/main/java/com/tesis2utp/hosplima/controller/HospitalController.java
+++ src/main/java/com/tesis2utp/hosplima/controller/HospitalController.java
@@ -5,27 +5,25 @@ import com.tesis2utp.hosplima.model.Hospital;
 import com.tesis2utp.hosplima.service.impl.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/hospitals")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class HospitalController {
 
     @Autowired
     private HospitalService hospitalService;
 
     @GetMapping("/hospitalMapa")
-    public ResponseEntity<List<HospitalMapa>> getAllHospitalMapa (){
+    public ResponseEntity<List<HospitalMapa>> getAllHospitalMapa() {
         return ResponseEntity.ok().body(this.hospitalService.getAllHospitalMapa());
     }
 
     @GetMapping("/hospital/{ipress}")
-    public ResponseEntity<Hospital> getHospitalByIpress (@PathVariable String ipress){
+    public ResponseEntity<Hospital> getHospitalByIpress(@PathVariable String ipress) {
         //Hospital hospital = this.hospitalService.getHospitalByIpress(ipress);
         return ResponseEntity.ok().body(this.hospitalService.getHospitalByIpress(ipress));
     }
