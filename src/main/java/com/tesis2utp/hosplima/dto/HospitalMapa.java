8dc29926b5601362e0912dce2cd9210eae2be8b3
carldevoos <carl.devoos@gmail.com>
2021-06-24 00:18:03 -0500

    Se añade nombre de hospital en HospitalMapa

--- src/main/java/com/tesis2utp/hosplima/dto/HospitalMapa.java
+++ src/main/java/com/tesis2utp/hosplima/dto/HospitalMapa.java
@@ -3,14 +3,16 @@ package com.tesis2utp.hosplima.dto;
 public class HospitalMapa {
 
     private String ipress;
+    private String nombre;
     private Double longitud;
     private Double latitud;
 
     public HospitalMapa() {
     }
 
-    public HospitalMapa(String ipress, Double longitud, Double latitud) {
+    public HospitalMapa(String ipress, String nombre, Double longitud, Double latitud) {
         this.ipress = ipress;
+        this.nombre = nombre;
         this.longitud = longitud;
         this.latitud = latitud;
     }
@@ -21,6 +23,14 @@ public class HospitalMapa {
 
     public void setIpress(String ipress) {
         this.ipress = ipress;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public Double getLongitud() {
